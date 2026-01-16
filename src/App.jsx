9672ce5bc8294fd960dbb57b16483451cdc211e6
Stanley Delva <juniordelva1@gmail.com>
2026-01-15 23:04:14 -0500

    browser router implemented, along with the home, about, and company page components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import {
+    Link,
+    Routes,
+    Route,
+    useParams,
+    BrowserRouter,
+    Outlet,
+} from "react-router-dom";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Layout />}>
+                        <Route index element={<Home />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/company" element={<Company />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
-export default App
+function Layout() {
+    return (
+        <div className="layout">
+            <Navbar />
+
+            <main>
+                <Outlet />
+            </main>
+        </div>
+    );
+}
+
+function Navbar() {
+    return (
+        <>
+            <div className="navbar">
+                <Link to="/">Home</Link>
+                <Link to="/about">About </Link>
+                <Link to="/company">Company </Link>
+            </div>
+        </>
+    );
+}
+
+function About() {
+    return (
+        <>
+            <div className="about-page">
+                <h1>This the about page </h1>
+            </div>
+        </>
+    );
+}
+function Home() {
+    return (
+        <>
+            <div className="home-page">
+                <h1>Home page</h1>
+            </div>
+        </>
+    );
+}
+
+function Company() {
+    return (
+        <div className="company-page">
+            <h1>About the company</h1>
+        </div>
+    );
+}
+export default App;
